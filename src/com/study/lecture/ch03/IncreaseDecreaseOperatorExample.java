1408c8ea4014ab8cf05aaf4fc81549e7a7df21dd
HDHun <pooekdma@naver.com>
2022-02-09 17:32:44 +0900

    java

---
+++ src/com/study/lecture/ch03/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,38 @@
+package com.study.lecture.ch03;
+
+public class IncreaseDecreaseOperatorExample {
+
+	public static void main(String[] args) {	
+//	++ : 1증감
+//	-- : 1감소
+	
+//	++x; : 피연산자를 1 증가시키고 다른연산 수행
+//	x++; : 다른 연산 수행 후 피연산자 1 증가
+//  다른 연산자가 없으면 앞에 있나 뒤에 있나 결과 같음	
+	
+		int x = 10;	
+		int y = 10;
+		int z;
+		
+		
+		
+		++x;
+		++x;
+		y--;
+		--y;
+
+		System.out.println("y=" + y);
+		
+		z = x++;
+		z= ++x;
+		System.out.println("z=" + z);
+		System.out.println("x=" + x);
+		
+		z= ++x + y++; 14 8
+		System.out.println("z=" + z); 15 8
+		System.out.println("x=" + x); 15
+		System.out.println("y=" + y); 9
+		
+	}
+
+}
---
+++ src/com/study/lecture/ch03/SignOperatorExample.java
@@ -0,0 +1,12 @@
+package com.study.lecture.ch03;
+
+public class SignOperatorExample {
+
+	public static void main(String[] args) {
+	
+		
+		
+			
+	}
+
+}
