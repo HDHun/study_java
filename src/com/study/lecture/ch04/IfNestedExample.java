52bff35134924e9817357bc54f637da001c2c4da
HDHun <pooekdma@naver.com>
2022-02-11 17:41:39 +0900

    java

--- src/com/study/lecture/ch04/IfNestedExample.java
+++ src/com/study/lecture/ch04/IfNestedExample.java
@@ -4,48 +4,69 @@ public class IfNestedExample {
 
 	public static void main(String[] args) {
 
-
-		
-		int score = 98;
+		int score = 50;
 		String grade = "";
-		
-		if(score >= 90) {
-			if(score >=95) {
+
+		if (score >= 90) {
+			if (score >= 95) {
 				grade = "A+";
 			} else {
 				grade = "A";
 			}
 		}
-		
-		if(score >= 80) {
-			if(score >= 85) {
+
+		if (score < 90) {
+			if (score >= 85) {
 				grade = "B+";
 			} else {
 				grade = "B";
 			}
 		}
-		
-		if(score >= 70) {
-			if(score >= 75) {
+
+		if (score < 80) {
+			if (score >= 75) {
 				grade = "C+";
 			} else {
 				grade = "C";
 			}
 		}
-		
-		if(score >= 60) {
-			if(score >= 65) {
+
+		if (score < 60) {
+			if (score >= 65) {
 				grade = "D+";
 			} else {
 				grade = "D";
-				
-			}s
-			System.out.println("grade:" +grade);
-		}
-		
-		
-		
 
+			}
+		}
+
+		if (score < 59) {
+			grade = "F";
+		}
+		System.out.println("grade:" + grade);
+
+
+		int x = -10;
+		int y = 10;
+		
+		if (x >=0) {
+			if (y >=0) {
+				System.out.println(1);
+			} else {
+				System.out.println(4);
+			}
+					}
+			else  (x < 0) {
+			if (y < 0) {
+					System.out.println(3);
+			} else {
+				System.out.println(2);	
+			}
+			
+						}
+
+
+		}
 	}
 
-}
+
