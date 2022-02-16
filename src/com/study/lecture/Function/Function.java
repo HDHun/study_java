ddd4a4f9d5aab609fef766e88beb43214baa8622
HDHun <pooekdma@naver.com>
2022-02-16 17:41:23 +0900

    java

---
+++ src/com/study/lecture/Function/Function.java
@@ -0,0 +1,64 @@
+package com.study.lecture.Function;
+
+public class Function {
+
+	public static void main(String[] args) {
+			
+//		함수의 사용
+		int sum = add(3,4);
+		System.out.println(sum);
+	}
+//		public 리턴자료형 메소드형 ( 입력자료형1 입력변수1, 입력자료형2 입력변수2, .args)
+//			...
+//			return 리턴값; // 리턴자료형이 void인 경우에는 return문이 필요없다.
+		
+		// 함수 선언
+		public static int add (int a, int b) {
+			
+			int sum = a + b;			
+			return sum;
+		}
+	
+		public static all(int a, int b) {
+			
+			
+			
+			return
+					
+		}
+		
+	
+		
+		
+		
+		
+		public int aaa (int a, int b, int c, String d, boolean e) {
+			System.out.println(a);
+			return 1;
+		}
+	
+		
+		
+		
+//		boolean
+//		byte
+//		short
+//		int
+//		long
+//		float
+//		double
+//		
+		
+		
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+
