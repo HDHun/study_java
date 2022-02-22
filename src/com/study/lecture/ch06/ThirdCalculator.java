4087f179d382fd591aa01037c18924f6d2c81a95
HDHun <pooekdma@naver.com>
2022-02-22 17:37:29 +0900

    java

--- src/com/study/lecture/ch06/ThirdCalculator.java
+++ src/com/study/lecture/ch06/ThirdCalculator.java
@@ -2,4 +2,18 @@ package com.study.lecture.ch06;
 
 public class ThirdCalculator {
 
+	
+	double areaRectangle(double a, double b) {
+		return a * b;
+	}
+	
+	double areaRectangle(double a) {
+		return a*a;
+	}
 }
+	
+	
+	
+	
+	
+
