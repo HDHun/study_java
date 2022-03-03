130218715239248bd42e4ce3f5ac298573aedbeb
HDHun <pooekdma@naver.com>
2022-03-03 17:37:52 +0900

    java

---
+++ src/com/urdomain/lecture/ch07/SmartPhoneExample.java
@@ -0,0 +1,18 @@
+package com.urdomain.lecture.ch07;
+
+public class SmartPhoneExample {
+
+	public static void main(String[] args) {
+
+		
+		
+		SmartPhone smartPhone = new SmartPhone("aaaa");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+		
+		
+	}
+
+}
