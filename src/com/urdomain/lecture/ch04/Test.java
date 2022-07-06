41a9d15624e4b9788816232876dd86ca2f67a5db
HDHun <pooekdma@naver.com>
2022-07-06 21:46:03 +0900

    다시보니 알겠군

--- src/com/urdomain/lecture/ch04/Test.java
+++ src/com/urdomain/lecture/ch04/Test.java
@@ -10,14 +10,13 @@ public class Test {
 		for(int i = 2; i < 10; i++ ) {
 			for(int j = 1; j < 10; j++) {
 				int sum = i*j;
-				int sum1 = 0;
 				
-				if(sum/2 == 0) {
+				if(sum/2 != 0) {
 				
-				System.out.println(sum + sum1);
+					System.out.println(sum);
 				
-			}
 				} else {
+			}
 					
 				}
 				
